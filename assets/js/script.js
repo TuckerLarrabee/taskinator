43696dbba0ab251dd3b56957d38a27860e554d83
TCL git <tuckerlarrabee13@gmail.com>
2021-12-27 16:39:20 -0500

    appending children

--- assets/js/script.js
+++ assets/js/script.js
@@ -230,6 +230,76 @@ var saveTasks = function () {
   localStorage.setItem("tasks", JSON.stringify(tasks));
 };
 
+//Gets task items from localStorage.
+
+// Converts tasks from the string format back into an array of objects.
+
+// Iterates through a tasks array and creates task elements on the page from it.
+
+var loadTasks = function () {
+  tasks = localStorage.getItem("tasks");
+
+  console.log(tasks);
+
+  if (tasks === null) {
+    tasks = [];
+    return false;
+  }
+
+  tasks = JSON.parse(tasks);
+  console.log(tasks);
+
+  for (var i = 0; i < tasks.length; i++) {
+    console.log(tasks[i]);
+    tasks[i].id = taskIdCounter;
+    console.log(tasks[i]);
+
+    var listItemEl = document.createElement("li");
+    listItemEl.className = "task-item";
+
+    //add task id as a custom attribute
+    listItemEl.setAttribute("data-task-id", tasks[i].id);
+    console.log(listItemEl);
+
+    //create div to hold task info and add to list item
+    var taskInfoEl = document.createElement("div");
+    taskInfoEl.className = "task-info";
+    taskInfoEl.innerHTML =
+      "<h3 class = 'task-name'>" +
+      tasks[i].name +
+      "</h3><span class = 'task-type'>" +
+      tasks[i].type +
+      "</span>";
+
+    listItemEl.appendChild(taskInfoEl);
+
+    var taskActionsEl = createTaskActions(tasks[i].id);
+    listItemEl.appendChild(taskActionsEl);
+    console.log(listItemEl);
+
+    if (tasks[i].status === "to-do") {
+      listItemEl.querySelector(
+        "select[name='status-change']"
+      ).selectedIndex = 0;
+      tasksToDoEl.appendChild(listItemEl);
+    } else if (tasks[i].status === "in progress") {
+      listItemEl.querySelector(
+        "select[name='status-change']"
+      ).selectedIndex = 1;
+      tasksInProgressEl.appendChild(listItemEl);
+    } else if (tasks[i].stauts === "complete") {
+      listItemEl.querySelector(
+        "select[name='status-change']"
+      ).selectedIndex = 2;
+      tasksCompletedEl.append(listItemEl);
+    }
+    taskIdCounter++;
+    console.log(listItemEl);
+  }
+};
+
 pageContentEl.addEventListener("click", taskButtonHandler);
 
 pageContentEl.addEventListener("change", taskStatusChangeHandler);
+
+loadTasks();
